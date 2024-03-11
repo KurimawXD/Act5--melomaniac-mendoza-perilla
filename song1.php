79186c56a58519bd619dc1202717b7a6070c3e45
MendozaJoshua <mendoza.joshua.malapad@gmail.com>
2024-03-11 17:02:16 +0800

    song1.php added

---
+++ song1.php
@@ -0,0 +1,205 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+    <style>
+        body {
+
+        	background-image: url(img/piano.jpg);
+        	background-size: cover;
+        	background-position: auto;
+            width: 100%;
+            height: 100vh;
+            margin: 0; 
+            padding: 0; 
+        }
+
+        
+        .text{
+            font-size: 20px;
+            color: white;
+            text-align: center;
+            font-weight: 80px;
+
+        }
+
+    </style>
+</head>
+<body>
+	<div class="text">
+	<h1> "TAHANAN"</h1>
+	<p>by: Adie</p>
+
+	<?php
+	echo "<pre>";
+	echo "<strong>";
+	echo "<br>";
+	echo "<span style='color:yellow'> C\t   Em</span><br>";
+	echo "Sa araw-araw<br>";
+	echo "<span style='color:yellow'>    Am</span><br>";
+	echo "tanging ikaw ang<br>";
+	echo "<span style='color:yellow'>F</span>                    <br>";
+	echo "palagi kong hinahangad<br>";
+	echo "<span style='color:yellow'> C\t   Em</span><br>";
+	echo "laging tanaw<br>";
+	echo "<span style='color:yellow'>\t Fmaj7</span><br>";
+	echo "Sa 'yo ang ilaw";	
+	echo "<br> <br>";
+	echo "Na nagsisilbi kong liwanag";
+	echo "<br> <br>";
+	echo "<br> <br>";
+	echo "<span style='color:yellow'>   C \t        Em \t            Fmaj7</span>\n";
+	echo "Labis ang ngiti kapag ika'y kaharap<br>";
+	echo "<span style='color:yellow'>\t                      C</span><br>";
+	echo "Ramdam ko ang pagmamahal giliw<br>";
+	echo "<span style='color:yellow'>\t        Em     \t             Fmaj7</span><br>";
+	echo "Namumukod-tangi ka at walang katulad<br>";
+	echo "<span style='color:yellow'>                       \t   C Em Fmaj7</span><br>";
+	echo "Ikaw lang ang para sa 'kin...";
+	echo "<br> <br>";
+	echo "<br> <br>";	
+	echo "<span style='color:yellow'>C     \t     Em      \t</span><br>";
+	echo "Sa 'yo lang, sa 'yo lang<br>";
+	echo "<span style='color:yellow'>  Fmaj7</span><br>";
+	echo "Ako uuwi";
+	echo "<br> <br>";
+	echo "Kaya naman";
+	echo "<br><br>";
+ 	echo "<br><br>";
+ 	echo "<span style='color:yellow'>C    \t   Em\t</span>    <br>";
+ 	echo "Dito ka sa piling ko<br>";
+ 	echo "<span style='color:yellow'>  Fmaj7</span><br>";
+	echo "O, dito ka lang";
+	echo "<br> <br>";
+	echo "Dito ka lang<br>";
+	echo "<span style='color:yellow'>C               Em\t</span>  <br>";
+	echo "Bumabagal ang ikot ng mundo<br>";
+	echo "<span style='color:yellow'>            Fmaj7</span><br>";
+	echo "Kapag ika'y nariyan";
+	echo "<br> <br>";
+	echo "O aking tahanan";
+	echo "<br> <br>";
+	echo "<br> <br>";
+	echo "<span style='color:yellow'>C           Em    Am F</span><br>";
+	echo"Ta ta ta ta ta tahanan<br>";
+	echo "<span style='color:yellow'>      C</span><br>";
+	echo "Dito ka lang<br>";
+	echo "<span style='color:yellow'>      Em</span><br>";
+	echo "Dito ka lang<br>";
+	echo "<span style='color:yellow'>         Am Fm</span><br>";
+	echo "Dito ka lang<br>";
+	echo "<span style='color:yellow'>G          C Em</span><br>";
+	echo "O aking tahanan";
+	echo "<br> <br>";
+	echo "<br> <br>";
+	echo "<span style='color:yellow'> C              Em</span><br>";
+	echo "Sa bawat sandali<br>";
+	echo " <span style='color:yellow'>    Am      F</span>              <br>";
+	echo "Na tayo ay magkayakap nang mahigpit<br>";
+	echo "<span style='color:yellow'>C             Em                    Am</span> <br>";
+	echo "Taglay mong init ang bumabalot sa 'king<br>";
+	echo "<span style='color:yellow'>F</span>        <br>";
+	echo "Nilalamig na damdamin";
+	echo "<br> <br>";
+	echo "<br> <br>";
+	echo "<span style='color:yellow'>Am          Em           F</span><br>";
+    echo "Tayong dalawa'y pinagtagpo<br>";
+    echo " <span style='color:yellow'>                   C</span><br>";
+	echo "Ng tamang pagkakataon<br>";
+	echo "<span style='color:yellow'> Em</span><br>";
+	echo "Hindi maitatanggi<br>";
+	echo "<span style='color:yellow'>Am            F</span><br>";
+	echo "Na sa akin ikaw ang tanging<br>";
+	echo "<span style='color:yellow'>  C Em Am F</span><br>";
+	echo "Tiyak ahh...";
+	echo "<br> <br>";
+	echo "<br> <br>";
+	echo "<span style='color:yellow'>    C                G                Am F</span><br>";
+	echo "Ikaw lang, ikaw lang ang tinatangi<br>";
+	echo "<span style='color:yellow'>     Am          Em</span>     <br>";
+	echo "Ikaw lang at ako ang<br>";
+	echo "<span style='color:yellow'>F</span>                    <br>";
+	echo "Naaaninag (naaaninag)<br>";
+	echo "<span style='color:yellow'> Am</span>      Em<br>";
+	echo "Sa gitna ng paraiso na<br>";
+	echo "<span style='color:yellow'>F</span>              <br>";
+	echo "Ating sinimulan<br>";
+	echo " <span style='color:yellow'>             G</span><br>";
+	echo "O aking tahanan";
+	echo "<br> <br>";
+	echo "<br> <br>";
+	echo "<span style='color:yellow'>C                  G</span>             <br>";
+ 	echo "Pinapawi lahat ng iyong mga ngiti<br>";
+ 	echo "<span style='color:yellow'>Am                 F</span>               <br>";
+	echo "Negatibo na nakadikit sa 'king labi<br>";
+	echo "<span style='color:yellow'>C                       G</span>           <br>";
+	echo "Huli ng iyong ngiti ang aking kiliti<br>";
+	echo "<span style='color:yellow'>Am                      F</span>          <br>";
+	echo "Katotohanan na hindi ko maitatanggi<br>";
+	echo "<span style='color:yellow'>Am</span><br>";
+	echo "Na mahal kita<br>";
+	echo "<span style='color:yellow'>Fm  G</span><br>";
+	echo "Walang iba";
+	echo "<br> <br>";
+	echo "Kaya naman";
+	echo "<br> <br>";
+	echo "<br> <br>";
+	echo "<span style='color:yellow'>C          Em</span>      <br>";
+	echo "Dito ka sa piling ko<br>";
+	echo "<span style='color:yellow'>Am</span>       <br>";
+	echo "O, dito ka lang<br>";
+	echo "<span style='color:yellow'>F</span>           <br>";
+	echo "Dito ka lang<br>";
+	echo "<span style='color:yellow'>C              Em</span>          <br>";
+	echo "Bumabagal ang ikot ng mundo<br>";
+	echo "<span style='color:yellow'>Am</span>    <br>";
+	echo "Kapag ika'y nariyan<br>";
+	echo "<span style='color:yellow'>F</span>       <br>";
+ 	echo "O aking tahanan<br>";
+	echo "<span style='color:yellow'>C           Em    Am F</span><br>";
+	echo "Ta ta ta ta ta tahanan...<br>";
+	echo "<span style='color:yellow'>      C</span><br>";
+	echo "Dito ka lang<br>";
+	echo "<span style='color:yellow'>      Em</span><br>";
+	echo "Dito ka lang<br>";
+	echo "<span style='color:yellow'>      F</span><br>";
+	echo "Dito ka lang<br>";
+	echo "<span style='color:yellow'>      C            Em      Am  F</span>    <br>";
+	echo "Dito ka sa piling ko ohh<br>";
+	echo "(Pinapawi lahat ng iyong ngiti<br>";
+	echo "Negatibo na nakadikit sa 'king labi)";
+	echo "<br> <br>";
+	echo "<br> <br>";
+	echo "<span style='color:yellow'>C              Em        Am      F</span><br>";
+	echo "Bumabagal ang ikot ng mundo woah oh<br>";
+	echo "(Huli ng iyong ngiti ang aking kiliti<br>";
+	echo "Katotohanan na hindi ko maitatanggi)";
+	echo "<br> <br>";
+	echo "<br> <br>";
+	echo "<span style='color:yellow'>C           Em    Am F</span><br>";
+	echo "Ta ta ta ta ta tahanan<br>";
+	echo "<span style='color:yellow'>      C</span><br>";
+	echo "Dito ka lang<br>";
+	echo "<span style='color:yellow'>      Em</span><br>";
+	echo "Dito ka lang<br>";
+	echo "<span style='color:yellow'>           F Fm</span><br>";
+	echo "Dito ka lang<br>";
+	echo "<span style='color:yellow'>G      C</span>  <br>";
+	echo "O aking tahanan";
+	echo "<br><br>";
+	echo "Mmm...<br>";
+	echo "<br> <br>";
+	echo "<br> <br>";
+	echo "<br> <br>";
+	echo "</strong>";
+	echo "</pre>";
+	?>
+
+
+	</div>
+
+</body>
+
+</html>
