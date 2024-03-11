b4b1fd6927d6bc96c8a40be6ee1b247c9ac5bbf9
MendozaJoshua <mendoza.joshua.malapad@gmail.com>
2024-03-11 17:04:24 +0800

    song3.php added

---
+++ song3.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+    <style>
+        body {
+
+            background-image: url(img/piano.jpg);
+            background-size: cover;
+            background-position: justify;
+            width: 100%;
+            height: 100vh;
+            margin: 0; 
+            padding: 0; 
+        }
+
+        
+        .text{
+            font-size: 20px;
+            color: white;
+            text-align: center;
+            font-weight: 80px;
+
+        }
+
+    </style>
+</head>
+<body>
+	<div class="text">
+
+	<h1>"GITARA"</h1>
+	<p1>by Parokya ni Edgar</p1>
+
+	<?php
+	echo "<pre><br><br>";
+
+	echo "[Intro]<br>";
+	echo '<span style="color:yellow">G G/B C D</span><br><br>';
+
+	echo "[Verse 1]<br><br>";
+
+	echo '   <span style="color:yellow">G                    G/B</span>  <br>';
+	echo "Bakit pa kailangang magbihis<br>";
+	echo ' <span style="color:yellow">   C                 D</span>    <br>';
+	echo "Sayang din naman ang porma<br>";
+	echo ' <span style="color:yellow">   G                    G/B</span>  <br>';
+	echo "Lagi lang namang may sisingit<br>";
+	echo ' <span style="color:yellow">   C                 D</span>    <br>';
+	echo "Sa tuwing tayo'y magkasama<br><br><br>";
+
+	echo '    <span style="color:yellow">G                    G/B</span>  <br>';
+	echo "Bakit pa kelangan ang rosas<br>";
+	echo ' <span style="color:yellow">   C             D</span>         <br>';
+	echo "Kung marami namang mag-aalay sayo<br>";
+	echo ' <span style="color:yellow">   G               G/B</span>  <br>';
+	echo "Uupo na lang at aawit<br>";
+	echo '  <span style="color:yellow">  C             D</span>       <br>';
+	echo "Maghihintay ng pagkakataon<br><br><br>";
+
+	echo "[Chorus]<br><br>";
+
+	echo '<span style="color:yellow">    Am7                 D</span>    <br>';
+	echo "Hahayaan na lang silang<br>";
+	echo '<span style="color:yellow">          G           D/F#</span> Em<br>';
+	echo "Magkandarapa na manligaw sayo<br>";
+	echo "<span style='color:yellow'>    Am7      D          G </span>    D/F#  Em<br>";
+	echo "Idadaan na lang kita sa awitin kong ito<br>";
+	echo " <span style='color:yellow'>   Am7      D          G     D/F#</span>  E<br>";
+	echo "Sabay ang tugtog ng gitara<br>";
+	echo " <span style='color:yellow'>   Am7      D          G</span>    <br>";
+	echo "Idadaan na lang sa gitara<br><br><br>";
+
+	echo "[Verse 2]<br><br>";
+
+	echo " <span style='color:yellow'>   G                    G/B </span> <br>";
+	echo "Mapapagod lang sa kakatingin<br>";
+	echo "  <span style='color:yellow'>  C              D  </span>  <br>";
+	echo "Kung marami namang nakaharang<br>";
+	echo " <span style='color:yellow'>   G                    G/B </span> <br>";
+	echo "Aawit na lang at magpaparinig<br>";
+	echo "  <span style='color:yellow'>  C            D </span>  <br>";
+	echo "Nang lahat ng aking nadarama<br><br><br>";
+
+	echo "[Chorus]<br><br>";
+
+	echo " <span style='color:yellow'>   Am7                 D  </span>  <br>";
+	echo "Pagbibigyan na lang silang<br>";
+	echo " <span style='color:yellow'>   G                 D/F# Em </span>   <br>";
+	echo "Magkandarapa na manligaw sayo<br>";
+	echo "  <span style='color:yellow'>  Am7      D          G     D/F#  Em</span><br>";
+	echo "Idadaan na lang kita sa awitin kong ito<br>";
+	echo "  <span style='color:yellow'>  Am7      D          G     D/F#  E</span><br>";
+	echo "Sabay ang tugtog ng gitara<br>";
+	echo "   <span style='color:yellow'> Am7      D          G</span>    <br>";
+	echo "Idadaan na lang sa gitara<br><br><br>";
+	
+	echo " <span style='color:yellow'>   Am7                 D </span>   <br>";
+	echo "Pagbibigyan na lang silang<br>";
+	echo " <span style='color:yellow'>   G                 D/F# Em  </span>  <br>";
+	echo "Magkandarapa na manligaw sayo<br>";
+	echo " <span style='color:yellow'>   Am7      D          G     D/F#  Em</span><br>";
+	echo "Idadaan na lang kita sa awitin kong ito<br>";
+	echo " <span style='color:yellow'>   Am7      D          G     D/F#  E</span><br>";
+	echo "Sabay ang tugtog ng gitara ... Oooohhhh<br>";
+	echo "  <span style='color:yellow'>  Am7</span><br>";
+	echo "Idadaan na lang...<br>";
+	echo " <span style='color:yellow'> D          G</span><br>";
+	echo "Sa gitara<br><br><br>";
+	echo "[Outro]<br>";
+	echo "<span style='color:yellow'>G G/B C D</span><br><br>";
+
+	echo "</pre>";
+
+	?>
+	</div>
+
+</body>
+</html>>
