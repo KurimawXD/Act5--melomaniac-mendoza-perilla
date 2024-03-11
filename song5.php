e8d357778483cfb27dc4897022c2d304ec0e2b1d
MendozaJoshua <mendoza.joshua.malapad@gmail.com>
2024-03-11 17:06:18 +0800

    song5.php added

---
+++ song5.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+    <style>
+        body {
+
+            background-image: url(img/piano.jpg);
+            background-size: cover;
+            background-position: justify;
+            width: 100%;
+            height: 100vh;
+            margin: 0; 
+            padding: 0; 
+        }
+
+        
+        .text{
+            font-size: 20px;
+            color: white;
+            text-align: center;
+            font-weight: 80px;
+
+        }
+
+
+    </style>
+</head>
+<body>
+	<div class="text">
+
+	<h1>"You'll Be Safe Here"</h1>
+	<p1>by Rivermaya</p1>
+
+	<?php
+	echo "<pre>";
+	echo "<br><br>";
+
+	echo "[Verse 1]\n\n";
+
+    echo "<span style='color:yellow'>D                   A</span>\n";
+    echo "Nobody knows\n";
+    echo "<span style='color:yellow'>Bm                  G</span>\n";
+    echo "Just why we're here\n";
+    echo "<span style='color:yellow'>D                   A</span>\n";
+    echo "Could it be fate\n";
+    echo "<span style='color:yellow'>Bm                  G</span>\n";
+    echo "Or random circumstance\n\n\n";
+
+
+    echo "[Chorus]\n\n";
+
+    echo "<span style='color:yellow'>G                  D/F#      Em7</span>\n";
+    echo "At the right place\n";
+    echo "<span style='color:yellow'>G                  D/F#      Em7</span>\n";
+    echo "At the right time\n";
+    echo "<span style='color:yellow'>G                  D/F#      Em7</span>\n";
+    echo "Two roads intertwine\n";
+    echo "<span style='color:yellow'>G                  D/F#      Em7</span>\n";
+    echo "And if things go wrong\n";
+    echo "<span style='color:yellow'>G                  D/F#      Em7</span>\n";
+    echo "As they sometimes will\n";
+    echo "<span style='color:yellow'>G                  D/F#      Em7</span>\n";
+    echo "The road you're on\n";
+    echo "<span style='color:yellow'>G                  D/F#      Em7</span>\n";
+    echo "Will lead you still\n\n\n";
+
+
+    echo "[Chorus]\n\n";
+
+    echo "<span style='color:yellow'>                C</span>\n";
+    echo "Close your eyes\n";
+    echo "<span style='color:yellow'>                G</span>\n";
+    echo "Dry your tears\n";
+    echo "<span style='color:yellow'>             Dm         F</span>\n";
+    echo "'Coz when nothing seems clear\n";
+    echo "<span style='color:yellow'>G               C</span>\n";
+    echo "You'll be safe here\n";
+    echo "<span style='color:yellow'>               G</span>\n";
+    echo "From the sheer weight\n";
+    echo "<span style='color:yellow'>        Dm</span>\n";
+    echo "Of your doubts and fears\n";
+    echo "<span style='color:yellow'>      F</span>\n";
+    echo "Weary heart\n";
+    echo "<span style='color:yellow'>G                  F</span>\n";
+    echo "You'll be safe here\n\n\n";
+
+
+    echo "[Verse 2]\n\n";
+
+    echo "<span style='color:yellow'>Dm</span>\n";
+    echo "Remember how we laughed\n";
+    echo "<span style='color:yellow'>      Am        G</span>\n";
+    echo "Until we cried\n";
+    echo "<span style='color:yellow'>Dm</span>\n";
+    echo "At the most stupid things\n";
+    echo "<span style='color:yellow'>        Am      G</span>\n";
+    echo "Like we were so high\n";
+    echo "<span style='color:yellow'>Dm                            Am G</span>\n";
+    echo "But love was all that we were on\n";
+    echo "<span style='color:yellow'>         F</span>\n";
+    echo "We belong\n\n";
+
+    echo "And though the world would\n";
+    echo "<span style='color:yellow'>      Am         G</span>\n";
+    echo "Never understand\n";
+    echo "<span style='color:yellow'>Dm</span>\n";
+    echo "This unlikely union\n";
+    echo "<span style='color:yellow'>    Am            G</span>\n";
+    echo "And why it still stands\n";
+    echo "<span style='color:yellow'>Dm                     Am   G</span>\n";
+    echo "Someday we will be set free\n";
+    echo "<span style='color:yellow'>        F</span>\n";
+    echo "Pray and believe\n\n\n";
+
+
+    echo "[Chorus]\n\n";
+
+    echo "<span style='color:yellow'>           C             G</span>\n";
+    echo "When the light disappears\n";
+    echo "<span style='color:yellow'>                Dm           F</span>\n";
+    echo "And when this world's insincere\n";
+    echo "<span style='color:yellow'>G                  C</span>\n";
+    echo "You'll be safe here\n";
+    echo "<span style='color:yellow'>G                 Dm</span>\n";
+    echo "When nobody hears you scream\n";
+    echo "<span style='color:yellow'>    F</span>\n";
+    echo "I'll scream with you\n";
+    echo "<span style='color:yellow'>G                  C</span>\n";
+    echo "You'll be safe here\n\n\n";
+
+
+    echo "[Interlude]\n";
+    echo "<span style='color:yellow'>C - G - Dm - Am - G - F</span>\n\n\n";
+
+
+    echo "[Chorus]\n\n";
+
+    echo "<span style='color:yellow'>          C</span>\n";
+    echo "Save your eyes\n";
+    echo "<span style='color:yellow'>          G</span>\n";
+    echo "From your tears\n";
+    echo "<span style='color:yellow'>           Dm       F</span>\n";
+    echo "When everything's unclear\n";
+    echo "<span style='color:yellow'>G              C</span>\n";
+    echo "You'll be safe here\n";
+    echo "<span style='color:yellow'>               G</span>\n";
+    echo "From the sheer weight\n";
+    echo "<span style='color:yellow'>         Dm</span>\n";
+    echo "Of your doubts and fears\n";
+    echo "<span style='color:yellow'>        F     G</span>\n";
+    echo "Wounded heart\n\n";
+
+    echo "<span style='color:yellow'>            C        G</span>\n";
+    echo "When the light disappears\n";
+    echo "<span style='color:yellow'>               Dm           F</span>\n";
+    echo "And when this world's insincere\n";
+    echo "<span style='color:yellow'>G               C</span>\n";
+    echo "You'll be safe here\n\n\n";
+
+
+    echo "[Outro]\n\n";
+
+    echo "<span style='color:yellow'>         G              Dm</span>\n";
+    echo "When nobody hears you scream\n";
+    echo "<span style='color:yellow'>            F</span>\n";
+    echo "I'll scream with you\n";
+    echo "<span style='color:yellow'>G              C</span>\n";
+    echo "You'll be safe here\n\n";
+
+    echo "<span style='color:yellow'>       G</span>\n";
+    echo "In my arms\n";
+    echo "<span style='color:yellow'>             Dm</span>\n";
+    echo "Through the long cold night\n";
+    echo "<span style='color:yellow'>      F</span>\n";
+    echo "Sleep tight\n";
+    echo "<span style='color:yellow'>G              C</span>\n";
+    echo "You'll be safe here\n\n";
+
+    echo "<span style='color:yellow'>         G         Dm</span>\n";
+    echo "When no one understands\n";
+    echo "<span style='color:yellow'>     F</span>\n";
+    echo "I'll believe\n";
+    echo "<span style='color:yellow'>G</span>\n";
+    echo "You'll be safe,\n";
+    echo "<span style='color:yellow'>C</span>\n";
+    echo "You'll be safe\n";
+    echo "<span style='color:yellow'>G              F           C</span>\n";
+    echo "You'll be safe here, oohhh\n";
+    echo "<span style='color:yellow'>         G           F</span>\n";
+    echo "Put your heart in my hands\n";
+    echo "<span style='color:yellow'>G              C</span>\n";
+    echo "You'll be safe here\n";
+    echo "</pre>";
+	?>
+	</div>>
+
+
+</body>
+</html>>
