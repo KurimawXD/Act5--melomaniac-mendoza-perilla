b45c8b226d61d85967e4f738f611d1984e35ff7c
MendozaJoshua <mendoza.joshua.malapad@gmail.com>
2024-03-11 17:05:25 +0800

    song4.php added

---
+++ song4.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+    <style>
+    	body {
+
+            background-image: url(img/piano.jpg);
+            background-size: cover;
+            background-position: justify;
+            width: 100%;
+            height: 100vh;
+            margin: 0; 
+            padding: 0; 
+        }
+        
+        .text{
+            font-size: 20px;
+            color: white;
+            text-align: center;
+            font-weight: 80px;
+
+        }
+
+
+    </style>
+</head>
+<body>
+	<div class="text">
+
+	<h1>"BINHI"</h1>
+	<p1>by Arthur Nery</p1>
+
+	<?php
+	echo "<pre>";
+	echo "<br>";
+	echo "<br>";
+	echo "[Intro]<br>";
+	echo "<span style='color:yellow'>Bb  Bbm</span><br><br>";
+	echo "[Verse 1]<br><br>";
+	echo "<span style='color:yellow'>    Dm Am            Gm</span><br>";
+	echo "Di ko na nadiligan, ang binhi ng iyong pagmamahal<br>";
+	echo "<span style='color:yellow'>    Dm Am            Gm</span><br>";
+	echo "Ayoko nang sapilitang ibuhos ang lahat ng dinadamdam<br>";
+	echo "<span style='color:yellow'>                Dm            Am</span><br>";
+	echo "Ang tangi kong hiling ay mahawakan<br>";
+	echo "<span style='color:yellow'>Gm</span><br>";
+	echo "Ang iyong mga kamay at daliri habang<br>";
+	echo "<span style='color:yellow'>                Dm            Am</span><br>";
+	echo "Dahan-dahang haplusin ng mga salita<br>";
+	echo "<span style='color:yellow'>   Gm</span><br>";
+	echo "Ang puso mong sabik mayakap 'pag nag-iisa<br><br><br>";
+
+
+	echo "[Chorus]<br><br>";
+	echo "<span style='color:yellow'>          F</span><br>";
+	echo "Kaya tahan na<br>";
+	echo " <span style='color:yellow'>        A7</span><br>";
+	echo "Sumandal ka<br>";
+	echo " <span style='color:yellow'>         Dm             C</span><br>";
+	echo "Hayaan mo na aking paglaruan<br>";
+	echo "  <span style='color:yellow'>             Bb           Bbm</span><br>";
+	echo "Apoy ng 'yong labi o paraluman<br><br><br>";
+
+
+	echo "[Verse 2]<br><br>";
+	echo "<span style='color:yellow'>         A7</span><br>";
+	echo "Binibining natutulog<br>";
+	echo "<span style='color:yellow'>Am           Gm </span>             <br>";
+	echo "Sa ilalim ng aking mga bulaklak<br>";
+	echo "<span style='color:yellow'>                   Dm       Am</span><br>";
+	echo "'Di mababaon sa limot ang<br>";
+	echo "<span style='color:yellow'>Gm  </span>       <br>";
+	echo "Ligayang hatid ng 'yong halimuyak<br><br><br>";
+
+	echo "<span style='color:yellow'>                Dm            Am</span><br>";
+	echo "Alak lamang ang pamunas sa natira<br>";
+	echo " <span style='color:yellow'>               Gm</span><br>";
+	echo "Mong alaalang 'di kumupas<br>";
+	echo "   <span style='color:yellow'>                  Dm                Am</span><br>";
+	echo "At kahit na, ipilit ko mang ibalik pa ang dati<br>";
+	echo "<span style='color:yellow'>Gm   </span>   <br>";
+	echo "Tayo'y mawawala pa rin<br><br><br>";
+
+
+	echo "[Chorus]<br><br>";
+	echo "<span style='color:yellow'>          F</span><br>";
+	echo "Kaya tahan na<br>";
+	echo "<span style='color:yellow'>         A7</span><br>";
+	echo "Sumandal ka<br>";
+	echo "<span style='color:yellow'>          Dm             C</span><br>";
+	echo "Hayaan mo na aking paglaruan<br>";
+	echo "<span style='color:yellow'>               Bb           Bbm</span><br>";
+	echo "Apoy ng 'yong labi o paraluman<br>";
+	echo "<span style='color:yellow'>             F        A7</span><br>";
+	echo "Ilang araw nang nakahiga<br>";
+	echo "<span style='color:yellow'>        Dm                 C</span><br>";
+	echo "Tuluyan na nga bang ako'y 'yong nilisan<br>";
+	echo " <span style='color:yellow'>             Bb              Bbm</span><br>";
+	echo "Kahit saglit pwede bang mahawakan?<br><br><br>";
+
+	echo "[Bridge]<br><br>";
+	echo "<span style='color:yellow'>F</span>                      <br>";
+	echo "'Di na kailangang lumayo<br>";
+	echo "<span style='color:yellow'>A7</span>             <br>";
+	echo "Halika sa akin<br>";
+	echo "<span style='color:yellow'>Dm             C</span>     <br>";
+	echo "'Di na muling mabibigo<br>";
+	echo "<span style='color:yellow'>Bb          Bbm</span><br>";
+	echo "Ako ay yakapin<br><br><br>";
+
+	echo "[Chorus]<br><br>";
+	echo "<span style='color:yellow'>          F</span><br>";
+	echo "Kaya tahan na<br>";
+	echo "<span style='color:yellow'>         A7</span><br>";
+	echo "Sumandal ka<br>";
+	echo "<span style='color:yellow'>          Dm             C</span><br>";
+	echo "Hayaan mo na aking paglaruan<br>";
+	echo "<span style='color:yellow'>               Bb           Bbm</span><br>";
+	echo "Apoy ng 'yong labi o paraluman<br>";
+	echo " <span style='color:yellow'>            F        A7</span><br>";
+	echo "Ilang araw nang nakahiga<br>";
+	echo " <span style='color:yellow'>       Dm                 C</span><br>";
+	echo "Tuluyan na nga bang ako'y 'yong nilisan<br>";
+	echo "<span style='color:yellow'>              Bb              Bbm</span><br>";
+	echo "Kahit saglit pwede bang mahawakan?<br><br><br>";
+	echo "</pre>";
+	?>
+ 	</div>
+</body>
+</html>
