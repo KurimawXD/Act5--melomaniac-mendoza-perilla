b9c606ba12c84c432a423c9f46f082af8dd15f58
MendozaJoshua <mendoza.joshua.malapad@gmail.com>
2024-03-11 17:07:01 +0800

    song6.php added

---
+++ song6.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+	    <style>
+    	body {
+
+            background-image: url(img/piano.jpg);
+            background-size: cover;
+            background-position: justify;
+            width: 100%;
+            height: 100vh;
+            margin: 0; 
+            padding: 0; 
+        }
+        
+        .text{
+            font-size: 20px;
+            color: white;
+            text-align: center;
+            font-weight: 80px;
+
+        }
+
+
+    </style>
+</head>
+<body>
+	<div class="text">
+
+	<h1>"Always"</h1>
+	<p1>by Daniel Ceasar</p1>
+
+	<?php
+	echo "<pre>";
+	echo "<br><br>";
+    echo "[Intro]\n";
+    echo "<span style='color:yellow'>A E/G# F#m7</span>\n";
+    echo "<span style='color:yellow'>D D/E E</span>\n\n\n";
+
+    echo "[Verse 1]\n\n";
+    echo "<span style='color:yellow'>A          E/G#</span>\n";
+    echo " Baby, baby\n";
+    echo "<span style='color:yellow'>                    F#m7</span>\n";
+    echo " There will always be space for you and me\n";
+    echo "<span style='color:yellow'>   D                  D/E E</span>\n";
+    echo " Right where you left it\n";
+    echo "<span style='color:yellow'>A             E/G#</span>\n";
+    echo " And just maybe\n";
+    echo "<span style='color:yellow'>                      D</span>\n";
+    echo " Enough time will pass\n\n";
+    echo " We'll look back and laugh\n";
+    echo "<span style='color:yellow'>  D             D/E E</span>\n";
+    echo " Just don't forget it\n\n\n";
+
+    echo "Continue with the rest of the lyrics...\n\n\n";
+
+    echo "[Outro]\n\n";
+    echo "<span style='color:yellow'>G   D          A                      F#m</span>\n";
+    echo " Always, can count on it sure as the stars in the sky\n";
+    echo "<span style='color:yellow'>G   D              A                          F#m</span>\n";
+    echo " Always, you can count on it as sure that the sun will rise\n";
+    echo "<span style='color:yellow'>G   D          A               F#m</span>\n";
+    echo " Always, my love for you ain't goin' nowhere\n";
+    echo "<span style='color:yellow'>G   D            A  E/G# F#m7 D D/E</span>\n";
+    echo " Always, I will be here\n";
+    echo "</pre>";
+	?>
+
+
+	</div>
+</body>
+</html>>
