63dc4624a36d73ef9346b66266ca7345c3e6f0c0
MendozaJoshua <mendoza.joshua.malapad@gmail.com>
2024-03-11 17:07:39 +0800

    song7.php added

---
+++ song7.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+	    <style>
+    	body {
+
+            background-image: url(img/piano.jpg);
+            background-size: cover;
+            background-position: justify;
+            width: 100%;
+            height: 100vh;
+            margin: 0; 
+            padding: 0; 
+        }
+        
+        .text{
+            font-size: 20px;
+            color: white;
+            text-align: center;
+            font-weight: 80px;
+
+        }
+    </style>
+</head>
+<body>
+	<div class="text">
+
+	<h1>"Pagsamo"</h1>
+	<p1>by Arthur Nery</p1>
+
+
+	<?php
+	echo "";
+	echo "<pre>";
+	echo "<br><br>";
+    echo "[Intro]\n";
+    echo "<span style='color:yellow'>C Em Bb F Fm</span>\n\n\n";
+
+    echo "[Verse 1]\n\n";
+    echo "Kung bibitaw nang mahinahon\n";
+    echo "<span style='color:yellow'>     C                     Em</span>\n";
+    echo "Ako ba'y lulubayan ng ating\n";
+    echo "<span style='color:yellow'>            Bb</span>\n";
+    echo "Mga kahapon na 'di na kayang\n";
+    echo "<span style='color:yellow'>            F  Fm</span>\n";
+    echo "Ayusin ng lambing\n";
+    echo " <span style='color:yellow'>             C               Em</span>\n";
+    echo "Mga pangako ba'y sapat na\n";
+    echo "<span style='color:yellow'>                     Bb</span>       \n";
+    echo "Upang muli tayong ipagtagpo ng \n";
+    echo "<span style='color:yellow'>    F   Fm</span>\n";
+    echo "hinaharap\n\n\n";
+
+    echo "[Pre-Chorus]\n\n";
+    echo "<span style='color:yellow'>       C   Em               Bb</span>  \n";
+    echo "Ba't pa ipapaalala 'di rin naman \n";
+    echo "<span style='color:yellow'>    F</span>\n";
+    echo "panghahawakan\n";
+    echo " <span style='color:yellow'>      C   Em               Bb</span>     \n";
+    echo "Ba't pa ipipilit kung 'di naman tayo\n";
+    echo "<span style='color:yellow'> F</span>\n";
+    echo "ang\n\n\n";
+
+    echo "[Chorus]\n\n";
+    echo "<span style='color:yellow'>            C           Em</span>\n";
+    echo "Para sa isa't-isa ooh\n";
+    echo "<span style='color:yellow'>               Bb             F</span>\n";
+    echo "'Di ba sinta tayong dalawa lang noon\n";
+    echo "<span style='color:yellow'>            C           Em</span>\n";
+    echo "Para sa isa't-isa hohh\n";
+    echo "<span style='color:yellow'>               Bb            F</span>  \n";
+    echo "Ba't 'di sumang-ayon sa 'tin ang \n";
+    echo "<span style='color:yellow'> Fm</span>\n";
+    echo "panahon\n\n\n";
+
+    echo "[Verse 2]\n\n";
+    echo " <span style='color:yellow'>  C</span>\n";
+    echo "Siguro nga'y wala nang natira\n";
+    echo "<span style='color:yellow'>   Em</span>\n";
+    echo "Sa mga sinulat mo na para sa 'kin\n";
+    echo "<span style='color:yellow'>   Bb</span>\n";
+    echo "Alam kong luha ang bumubura\n";
+    echo " <span style='color:yellow'>  F</span>\n";
+    echo "Ngunit hayaan mo na lang\n\n\n";
+
+    echo "[Verse 3]\n\n";
+    echo " <span style='color:yellow'>                 C</span>\n";
+    echo "Walang saysay ang panalangin ko\n";
+    echo "<span style='color:yellow'>                 Em</span>\n";
+    echo "Kung 'di ako ang hahanapin mo\n";
+    echo "<span style='color:yellow'>                 Bb</span>\n";
+    echo "Kahit sigaw pa ang pagsamo ko sa 'yo\n";
+    echo "<span style='color:yellow'>                 F</span>\n";
+    echo "Bakit 'di mo dama 'to\n\n\n";
+
+    echo "[Pre-Chorus]\n\n";
+    echo "<span style='color:yellow'>       C   Em               Bb</span>  \n";
+    echo "Ba't pa ipapaalala 'di rin naman \n";
+    echo "<span style='color:yellow'>    F</span>\n";
+    echo "panghahawakan\n";
+    echo " <span style='color:yellow'>      C   Em               Bb</span>     \n";
+    echo "Ba't pa ipipilit kung 'di naman tayo\n";
+    echo "<span style='color:yellow'> F</span>\n";
+    echo "ang\n\n\n";
+
+    echo "[Chorus]\n\n";
+    echo "<span style='color:yellow'>            C           Em</span>\n";
+    echo "Para sa isa't-isa ooh\n";
+    echo "<span style='color:yellow'>               Bb             F</span>\n";
+    echo "'Di ba sinta tayong dalawa lang noon\n";
+    echo "<span style='color:yellow'>            C           Em</span>\n";
+    echo "Para sa isa't-isa hohh\n";
+    echo "<span style='color:yellow'>              Bb             F</span>\n";
+    echo "Ba't 'di sumang-ayon sa 'tin ang \n";
+    echo "panahon\n\n";
+
+    echo "</pre>";
+	?>
+
+	</div>
+</body>
+</html>>
