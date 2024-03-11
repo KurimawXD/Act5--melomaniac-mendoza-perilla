b1e10692e0eec009e4c4150eeb090be8c37949be
MendozaJoshua <mendoza.joshua.malapad@gmail.com>
2024-03-11 17:03:34 +0800

    song2.php added

---
+++ song2.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+    <style>
+        body {
+
+            background-image: url(img/piano.jpg);
+            background-size: cover;
+            background-position: justify;
+            width: 100%;
+            height: 100vh;
+            margin: 0; 
+            padding: 0; 
+        }
+
+        
+        .text{
+            font-size: 20px;
+            color: white;
+            text-align: center;
+            font-weight: 80px;
+
+        }
+
+
+    </style>
+</head>
+<body>
+	<div class="text">
+
+	<h1>"YELLOW"</h1>
+	<p>by Coldplay</p>
+
+
+	<?php
+	echo "<pre>";
+	echo "<br><br>";
+	echo "[Verse]\n\n";
+    echo "<span style='color:yellow'>B                 F#        E              B Badd4 B</span>\n";
+    echo "Look at the stars, look how they shine for you\n";
+    echo "<span style='color:yellow'>B                 F#        E              B Badd4 B</span>\n";
+    echo "And everything you do, yeah, they were all yellow\n";
+    echo "<span style='color:yellow'>B                 F#        E              B Badd4 B</span>\n";
+    echo "I came along, I wrote a song for you\n";
+    echo "<span style='color:yellow'>B                 F#        E              B Badd4 B</span>\n";
+    echo "And all the things you do, and it was called 'Yellow'\n\n\n";
+    echo "[Chorus]\n\n";
+    echo "<span style='color:yellow'>E            G#m            F#               E</span>\n";
+    echo "Your skin, oh yeah, your skin and bones\n";
+    echo "<span style='color:yellow'>E         G#m            F#            E</span>\n";
+    echo "Turn into something beautiful, and you know\n";
+    echo "<span style='color:yellow'>E       G#m            F#               E</span>\n";
+    echo "You know I love you so\n";
+    echo "<span style='color:yellow'>E            B</span>       \n";
+    echo "You know I love you so\n\n\n";
+    echo "[Verse]\n\n";
+    echo "<span style='color:yellow'>B                 F#        E              B Badd4 B</span>\n";
+    echo "I swam across, I jumped across for you\n";
+    echo "<span style='color:yellow'>B                 F#        E              B Badd4 B</span>\n";
+    echo "Oh, what a thing to do, 'cause you were all yellow\n";
+    echo "<span style='color:yellow'>B                 F#        E              B Badd4 B</span>\n";
+    echo "I drew a line, I drew a line for you\n";
+    echo "<span style='color:yellow'>B                 F#        E              B Badd4 B</span>\n";
+    echo "Oh, what a thing to do, and it was all yellow\n\n\n";
+    echo "[Chorus]\n\n";
+    echo "<span style='color:yellow'>E            G#m            F#               E</span>\n";
+    echo "Your skin, oh yeah, your skin and bones\n";
+    echo "<span style='color:yellow'>E         G#m            F#            E</span>\n";
+    echo "Turn into something beautiful, and you know\n";
+    echo "<span style='color:yellow'>E       G#m            F#               E</span>\n";
+    echo "You know I love you so\n";
+    echo "<span style='color:yellow'>E            B</span>       \n";
+    echo "You know I love you so\n\n\n";
+    echo "[Bridge]\n\n";
+    echo "<span style='color:yellow'>G#m         F#             E</span>\n";
+    echo "For you I'd bleed myself dry\n\n\n";
+    echo "[Chorus]\n\n";
+    echo "<span style='color:yellow'>E            G#m            F#               E</span>\n";
+    echo "Your skin, oh yeah, your skin and bones\n";
+    echo "<span style='color:yellow'>E         G#m            F#            E</span>\n";
+    echo "Turn into something beautiful, and you know\n";
+    echo "<span style='color:yellow'>E       G#m            F#               E</span>\n";
+    echo "You know I love you so\n";
+    echo "<span style='color:yellow'>E            B</span>       \n";
+    echo "You know I love you so\n\n\n";
+    echo "[Outro]\n\n";
+    echo "<span style='color:yellow'>B                 F#        E              B Badd4 B</span>\n";
+    echo "Look how they shine for you, look how they shine for you\n";
+    echo "<span style='color:yellow'>B                 F#        E              B Badd4 B</span>\n";
+    echo "Look how they shine for you, look how they shine for...\n";
+    echo "<span style='color:yellow'>B                 F#        E              B Badd4 B</span>\n";
+    echo "Look how they shine for you, look how they shine for you\n";
+    echo "<span style='color:yellow'>B                 F#        E              B Badd4 B</span>\n";
+    echo "Look how they shine.\n";
+	echo "<br><br><br>";
+
+
+    echo "</pre>";
+
+	?>
+
+</body>
+</html>
